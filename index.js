8cbee353689b78186642ca869c9a7566eac2e529
job <akungajob47@gmail.com>
2023-03-30 10:48:54 +0300

    new stuff

---
+++ index.js
@@ -0,0 +1,21 @@
+
+// let number=20
+
+// if(number<10){
+//     console.log("number is less than 10")
+
+// }
+// else{
+//     console.log("number is greater than 10")
+// }
+
+let country="chad"
+let age = 18
+
+if(country=="togo"){
+    console.log("false")
+
+}
+else{
+    console.log("true")
+}
